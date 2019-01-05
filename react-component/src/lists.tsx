102b63dbb0c2867c1d85b50ce780800f878b6b30
whywaita <whywaita@whywrite.it>
2019-01-06 05:21:47 +0900

    Initial commit

---
+++ react-component/src/lists.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export class Lists extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      a: "string"
+    };
+  }
+
+  render() {
+    return <div>aaa</div>;
+  }
+}
---
+++ react-component/src/main.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Lists } from "./lists";
+
+ReactDOM.render(
+  <Lists />,
+
+  document.getElementById("root")
+);
